3a4a858513b603d73fb2f3f64531f49e07ea4d68
DungTran <tvdung182898@gmail.com>
2025-07-20 17:52:28 +0700

    vvvv

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -7,7 +7,7 @@ export function OPTIONS() {
   return new Response(null, {
     status: 204,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Hoặc FE domain cụ thể
+      'Access-Control-Allow-Origin': '*', 
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
